63347894bdc807cef246bb7738745bc9f74099f1
Hui-Chun Chieh <stella.chieh@gmail.com>
2023-06-03 13:03:00 -0400

    remove used files

--- src/controllers/responses/login.response.ts
+++
@@ -1,8 +0,0 @@
-import { BaseResponse } from "./base.response";
-
-export interface LoginResponseData {
-  token: string;
-  preferences: [string];
-}
-
-export type LoginResponse = BaseResponse<LoginResponseData>;
--- src/controllers/responses/register.response.ts
+++
@@ -1,7 +0,0 @@
-import { BaseResponse } from "./base.response";
-
-export interface RegisterResponseData {
-  token: string;
-}
-
-export type RegisterResponse = BaseResponse<RegisterResponseData>;
